00db276e037893555bb3b88fb78280b8cbf05793
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2018-10-17 09:35:04 +0100

    fixed db provision

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -5,10 +5,10 @@ sudo apt-get upgrade -y
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update -y
 sudo apt-get install -y mongodb-org=3.2.18 mongodb-org-server=3.2.18 mongodb-org-shell=3.2.18 mongodb-org-mongos=3.2.18 mongodb-org-tools=3.2.18
 
-sudo rm /etc/mongod.conf
-sudo ln -s /home/vagrant/environment/mongod.conf /etc/mongod.conf
+sudo rm -f /etc/mongod.conf
+sudo ln -s /home/vagrant/environment/db/mongod.conf /etc/mongod.conf
 
-sudo systemctl restart mongod
-sudo systemctl enable mongod
+sudo service mongod start
